3e8b1082412f322da5d77c946c57ba21742c315e
Natalie <113490096+nhfnnc@users.noreply.github.com>
2024-05-20 01:59:23 -0500

    Initial commit
    All functions should work minus 'set_DTR' and reading CNTR/OTR values less than 32bits wide.

--- src/LS7366R_Lib.cpp
+++ src/LS7366R_Lib.cpp
@@ -1,1 +1,139 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include "LS7366R_Lib.h"
 
+//Constructor
+LS7366R_Lib::LS7366R_Lib(int cs){
+    spiSpeed = 4000000;
+    chipSelect = cs;
+}
+
+void LS7366R_Lib::begin()
+{
+    pinMode(chipSelect, OUTPUT);
+    SPISettings comSettings(spiSpeed, bitOrder, spiMode);
+    SPI.begin();
+    SPI.beginTransaction(comSettings);
+    digitalWrite(chipSelect, HIGH);
+}
+
+void LS7366R_Lib::clear_MDR0()
+{
+    digitalWrite(chipSelect, LOW);
+    SPI.transfer(B00001000);
+    digitalWrite(chipSelect, HIGH);
+}
+
+void LS7366R_Lib::clear_MDR1()
+{
+    digitalWrite(chipSelect, LOW);
+    SPI.transfer(B00010000);
+    digitalWrite(chipSelect, HIGH);
+}
+
+void LS7366R_Lib::clear_CNTR()
+{
+    digitalWrite(chipSelect, LOW);
+    SPI.transfer(B00100000);
+    digitalWrite(chipSelect, HIGH);
+}
+
+void LS7366R_Lib::clear_STR()
+{
+    digitalWrite(chipSelect, LOW);
+    SPI.transfer(B00110000);
+    digitalWrite(chipSelect, HIGH);
+}
+
+uint8_t LS7366R_Lib::get_MDR0()
+{
+    digitalWrite(chipSelect, LOW);
+    SPI.transfer(B01001000);
+    uint8_t recieved = SPI.transfer(0x00);
+    return recieved;
+}
+
+uint8_t LS7366R_Lib::get_MDR1()
+{
+    digitalWrite(chipSelect, LOW);
+    SPI.transfer(B01010000);
+    uint8_t recieved = SPI.transfer(0x00);
+    return recieved;
+}
+
+uint32_t LS7366R_Lib::get_CNTR()
+{
+    digitalWrite(chipSelect, LOW);
+    SPI.transfer(B01100000);
+    byte n0 = SPI.transfer(0x00);
+    byte n1 = SPI.transfer(0x00);
+    byte n2 = SPI.transfer(0x00);
+    byte n3 = SPI.transfer(0x00);
+    uint32_t recieved = ((uint32_t)n0 << 24) | ((uint32_t)n1 << 16) | ((uint32_t)n2 << 8) | (uint32_t)n3;
+    digitalWrite(chipSelect, HIGH);
+    return recieved;
+}
+
+uint32_t LS7366R_Lib::get_OTR()
+{
+    digitalWrite(chipSelect, LOW);
+    SPI.transfer(B01101000);
+    byte n0 = SPI.transfer(0x00);
+    byte n1 = SPI.transfer(0x00);
+    byte n2 = SPI.transfer(0x00);
+    byte n3 = SPI.transfer(0x00);
+    uint32_t recieved = ((uint32_t)n0 << 24) | ((uint32_t)n1 << 16) | ((uint32_t)n2 << 8) | (uint32_t)n3;
+    digitalWrite(chipSelect, HIGH);
+    return recieved;
+}
+
+uint8_t LS7366R_Lib::get_STR()
+{
+    digitalWrite(chipSelect, LOW);
+    SPI.transfer(B01110000);
+    uint8_t recieved = SPI.transfer(0x00);
+    return recieved;
+}
+
+void LS7366R_Lib::set_MDR0(byte quad_mode, byte count_mode, byte index_mode, byte sync_mode, byte filter_mode)
+{
+    byte message = quad_mode + count_mode + index_mode + sync_mode + filter_mode;
+    digitalWrite(chipSelect, LOW);
+    SPI.transfer(B10001000);
+    SPI.transfer(message);
+    digitalWrite(chipSelect, HIGH);
+}
+
+void LS7366R_Lib::set_MDR1(byte byte_mode, byte ena_count, byte idx, byte cmp, byte bw, byte cy)
+{
+    byte message = byte_mode + ena_count + idx + cmp + bw + cy;
+    digitalWrite(chipSelect, LOW);
+    SPI.transfer(B10010000);
+    SPI.transfer(message);
+    digitalWrite(chipSelect, HIGH);
+}
+
+void LS7366R_Lib::set_DTR(uint32_t value)
+{
+    
+}
+
+
+void LS7366R_Lib::load_CNTR()
+{
+    digitalWrite(chipSelect, LOW);
+    SPI.transfer(B11100000);
+    digitalWrite(chipSelect, HIGH);
+}
+
+void LS7366R_Lib::load_OTR()
+{
+    digitalWrite(chipSelect, LOW);
+    SPI.transfer(B11101000);
+    digitalWrite(chipSelect, HIGH);
+}
+
+void LS7366R_Lib::set_Speed(int spd)
+{
+    spiSpeed = spd;
+}
--- src/LS7366R_Lib.h
+++ src/LS7366R_Lib.h
@@ -1,1 +1,77 @@
+#pragma once
+#include <Arduino.h>
+#include <SPI.h>
 
+
+//MDR0 options
+#define non_quad 0x0
+#define x1 0x1
+#define x2 0x2
+#define x4 0x3
+#define free_run 0x0
+#define single_cycle 0x4
+#define range_limit 0x8
+#define modulo_n 0xC
+#define disable_index 0x00
+#define CNTR_load 0x10
+#define CNTR_reset 0x20
+#define OTR_load 0x30
+#define async 0x00
+#define sync 0x40
+#define filter_1 0x00
+#define filter_2 0x80
+
+
+//MDR1 options
+#define byte_4 0x0
+#define byte_3 0x1
+#define byte_2 0x2
+#define byte_1 0x3
+#define count_ena 0x0
+#define count_dis 0x4
+#define nop 0x00
+#define flag_IDX 0x10
+#define flag_CMP 0x20
+#define flag_BW 0x40
+#define flag_CY 0x80
+
+
+//Constant SPI settings
+#define bitOrder MSBFIRST
+#define spiMode SPI_MODE0
+
+
+//A library for interfacing with the LS7366R quadrature encoder IC for Arudino.
+
+class LS7366R_Lib{
+    public:
+
+        LS7366R_Lib(int chip);
+        void begin();  //Prepares the LS7366R for communication with the previously set user parameters. (set_MDR0, set_MDR1, set_Speed)
+
+        void clear_MDR0();
+        void clear_MDR1();
+        void clear_CNTR();
+        void clear_STR();
+
+        uint8_t get_MDR0();
+        uint8_t get_MDR1();
+        uint32_t get_CNTR();
+        uint32_t get_OTR();
+        uint8_t get_STR();
+
+        void set_MDR0(byte quad_mode, byte count_mode, byte index_mode, byte sync_mode, byte filter_mode);
+        void set_MDR1(byte byte_mode, byte ena_count, byte idx, byte cmp, byte bw, byte cy);
+        void set_DTR(uint32_t value);
+
+        void load_CNTR();
+        void load_OTR();
+
+        void set_Speed(int spd);
+
+    private:
+
+        int spiSpeed;  //SPI communication speed in hz.
+        int chipSelect;
+
+};
